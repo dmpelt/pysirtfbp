24506b71ecb354ff3d422c043f76c382a24d6ad3
Daniel M. Pelt <D.M.Pelt@cwi.nl>
2016-04-14 14:30:41 +0200

    Remove supersampling in favor of strip kernel for filter calculation

--- sirtfbp/astra_plugin.py
+++ sirtfbp/astra_plugin.py
@@ -41,7 +41,7 @@ def mkdir_p(path):
             pass
         else: raise
 
-def getFilterFile(saveDir, proj_geom, ss, nIters, reg_grad, object_size):
+def getFilterFile(saveDir, proj_geom, nIters, reg_grad, object_size):
     """Creates a filename in ``saveDir`` that is unique
     to the combination of input parameters.
 
@@ -49,10 +49,10 @@ def getFilterFile(saveDir, proj_geom, ss, nIters, reg_grad, object_size):
     :type saveDir: :class:`string`
     :param proj_geom: ASTRA projection geometry.
     :type proj_geom: :class:`dict`
-    :param ss: Supersampling that is used.
-    :type ss: :class:`int`
     :param nIters: Number of iterations that is used.
     :type nIters: :class:`int`
+    :param object_size: Size of object that is reconstructed.
+    :type object_size: :class:`int`
     """
     hs = ""
     for i,j in sorted(proj_geom.items()):
@@ -67,7 +67,6 @@ def getFilterFile(saveDir, proj_geom, ss, nIters, reg_grad, object_size):
                 hs = hs + i + ": " + j.tostring().encode('ascii') + "; "
             except (AttributeError,UnicodeDecodeError):
                 hs = hs + i + ": " + str(j) + "; "
-    hs = hs + 'ss: ' + str(ss) + "; "
     hs = hs + 'nIters: ' + str(nIters) + "; "
     hs = hs + 'reg_grad: ' + str(reg_grad) + "; "
     hs = hs + 'object_size: ' + str(object_size) + "; "
@@ -81,13 +80,15 @@ class plugin(astra.plugin.ReconstructionAlgorithm2D):
     Options:
 
     'filter_dir': folder to cache computed filtes in
-    'ss' (optional): supersampling to use during filter computation
     'reg_grad' (optional): amount of l2 gradient minimization
     'object_size' (optional): if the scanned object is small compared to the
                               full field of view, use this parameter to set the
                               approximate size of the object in pixels. Note: it
                               is better to set this too high rather than too low,
                               setting it too low can result in major artifacts.
+    'ss' (optional): supersampling to use during filter computation. Note: this
+                     option is deprecated and will be ignored. It will be removed
+                     in the future.
     
     [1] Pelt, D. M., & Batenburg, K. J. (2015). Accurately approximating algebraic 
         tomographic reconstruction by filtered backprojection. In
@@ -109,16 +110,16 @@ class plugin(astra.plugin.ReconstructionAlgorithm2D):
             sf[i] = fftconvolve(padded,f[i],'same')[l:r]
         return (self.W.T*sf).reshape(self.v.shape)
     
-    def initialize(self, cfg, filter_dir, ss=8, reg_grad=None, object_size=None):
+    def initialize(self, cfg, filter_dir, ss=None, reg_grad=None, object_size=None):
         self.W = astra.OpTomo(self.pid)
         self.ang = self.pg['ProjectionAngles']
         self.fd = filter_dir
         mkdir_p(filter_dir)
         self.nd = self.pg['DetectorCount']
         if self.nd%2==0: self.nd += 1
-        self.ss = ss
+        if ss:
+            astra.log.warn("The 'ss' options has been deprecated and will be ignored")
         self.rg = reg_grad
-        self.iscuda = astra.projector.is_cuda(self.pid)
         if object_size:
             self.osz = int(object_size)
             if self.osz%2==0:
@@ -127,7 +128,7 @@ class plugin(astra.plugin.ReconstructionAlgorithm2D):
             self.osz = None
 
     def run(self, iterations):
-        self.fn = getFilterFile(self.fd, self.pg, self.ss, iterations, self.rg, self.osz)
+        self.fn = getFilterFile(self.fd, self.pg, iterations, self.rg, self.osz)
         if os.path.exists(self.fn):
             flt = np.load(self.fn)
             self.v[:] = self.customFBP(flt)
@@ -140,28 +141,19 @@ class plugin(astra.plugin.ReconstructionAlgorithm2D):
         na = len(self.ang)
         pgc = astra.create_proj_geom('parallel',1.0,nd,self.ang)
         vgc = astra.create_vol_geom((nds,nds))
-        if not self.iscuda:
-            pidc = astra.create_projector('strip',pgc,vgc)
+        pidc = astra.create_projector('strip',pgc,vgc)
         x = np.zeros((nds,nds))
         xs = np.zeros((nds,nds))
         sf = np.zeros((na,nd))
         vid = astra.data2d.create('-vol',vgc)
         sid = astra.data2d.create('-sino',pgc)
-        if self.iscuda:
-            cfg = astra.astra_dict('FP_CUDA')
-            cfg['options'] = {'DetectorSuperSampling':self.ss}
-        else:
-            cfg = astra.astra_dict('FP')
-            cfg['ProjectorId']=pidc
+        cfg = astra.astra_dict('FP')
+        cfg['ProjectorId']=pidc
         cfg['ProjectionDataId']=sid
         cfg['VolumeDataId']=vid
         fpid = astra.algorithm.create(cfg)
-        if self.iscuda:
-            cfg = astra.astra_dict('BP_CUDA')
-            cfg['options'] = {'PixelSuperSampling':self.ss}
-        else:
-            cfg = astra.astra_dict('BP')
-            cfg['ProjectorId']=pidc
+        cfg = astra.astra_dict('BP')
+        cfg['ProjectorId']=pidc
         cfg['ProjectionDataId']=sid
         cfg['ReconstructionDataId']=vid
         bpid = astra.algorithm.create(cfg)
@@ -194,5 +186,4 @@ class plugin(astra.plugin.ReconstructionAlgorithm2D):
         astra.algorithm.delete([vid,sid])
         np.save(self.fn,flt)
         self.v[:] = self.customFBP(flt)
-        if not self.iscuda:
-            astra.projector.delete(pidc)
+        astra.projector.delete(pidc)
